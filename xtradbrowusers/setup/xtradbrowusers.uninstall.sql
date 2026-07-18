a26c7ec448cb0f9d700074308e5b556aaa7d962b
webitproff <webitproff@gmail.com>
2026-07-18 09:58:22 +0300

    Enhance xtradbrowusers.uninstall.sql with comments
    Added metadata and comments to the uninstall SQL script.

--- xtradbrowusers/setup/xtradbrowusers.uninstall.sql
+++ xtradbrowusers/setup/xtradbrowusers.uninstall.sql
@@ -1,5 +1,17 @@
--- xtradbrowusers
 -- plugins/xtradbrowusers/setup/xtradbrowusers.uninstall.sql
 -- Удаляет таблицу cot_xtradbrowusers при деинсталляции плагина.
+-- Удаляем таблицу переводов первой, затем основную (из-за внешнего ключа)
 
+-- 
+-- Custom Extrafields Users i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+-- 
+-- Date: Jul 18, 2026
+-- package xtradbrowusers
+-- version 1.1.1
+-- author webitproff
+-- copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowusers-cotonti
+-- license BSD
+-- 
+
+DROP TABLE IF EXISTS `cot_xtradbrowusers_i18n`;
 DROP TABLE IF EXISTS `cot_xtradbrowusers`;
