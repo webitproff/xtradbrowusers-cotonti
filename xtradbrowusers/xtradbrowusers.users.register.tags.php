146c287167ef49bcc9e201586b5b6de8ccf6fdae
webitproff <webitproff@gmail.com>
2026-07-18 10:14:11 +0300

    Update xtradbrowusers.users.register.tags.php

--- xtradbrowusers/xtradbrowusers.users.register.tags.php
+++ xtradbrowusers/xtradbrowusers.users.register.tags.php
@@ -10,9 +10,12 @@
  * Хук users.register.tags. Отображает все extrafields плагина xtradbrowusers
  * с их пустыми или предзаполненными значениями в форме регистрации.
  *
- * Date: Jul 16, 2026
+ *
+ * Custom Extrafields Users i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+ *
+ * Date: Jul 18, 2026
  * @package xtradbrowusers
- * @version 1.0.0
+ * @version 1.1.1
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowusers-cotonti
  * @license BSD
