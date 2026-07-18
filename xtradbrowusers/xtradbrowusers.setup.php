a83bf693ea0cd20dc3ec9cd84d4392f0b8ee4285
webitproff <webitproff@gmail.com>
2026-07-18 09:53:36 +0300

    Update xtradbrowusers.setup.php

--- xtradbrowusers/xtradbrowusers.setup.php
+++ xtradbrowusers/xtradbrowusers.setup.php
@@ -2,12 +2,11 @@
 /* ====================
 [BEGIN_COT_EXT]
 Code=xtradbrowusers
-Name=Custom Extrafields Users
-Description=Custom Extrafields for Users module stored in own table. Дополнительные поля пользователей в собственной таблице.
-Version=1.0.1
-Date=Jul 16, 2026
+Name=Custom Extrafields Users i18n
+Description=Custom Extrafields for Users module stored in own table.
+Date=Jul 18, 2026
 Author=webitproff
-Copyright=Copyright (c) 2026 webitproff
+Copyright=Copyright (c) 2026 webitproff https://github.com/webitproff/
 Notes=BSD License
 Auth_guests=R
 Lock_guests=12345A
@@ -15,22 +14,35 @@ Auth_members=RW
 Lock_members=
 Requires_modules=users
 [END_COT_EXT]
+
+
+[BEGIN_COT_EXT_CONFIG]
+xtradbrowusers_i18n_use=01:radio::0:Мультиязычность полей активировать и использовать
+xtradbrowusers_i18n_lang_code_default=02:string::ru:Код основного языка сайта (должен совпадать с <code>$cfg['defaultlang']</code>)
+xtradbrowusers_i18n_lang_code_first=03:string::en:Код первого дополнительного языка
+xtradbrowusers_i18n_lang_code_first_use=04:radio::1:Использовать первый дополнительный язык
+xtradbrowusers_i18n_lang_code_second=05:string::ua:Код второго дополнительного языка
+xtradbrowusers_i18n_lang_code_second_use=06:radio::1:Использовать второй дополнительный язык
+[END_COT_EXT_CONFIG]
 ==================== */
 
 defined('COT_CODE') or die('Wrong URL');
 
+
 /**
  * xtradbrowusers.setup.php - Register data in $db_core and $db_config. Setup & Config File for the Plugin xtradbrowusers
  *
- * xtradbrowusers plugin for Cotonti 0.9.26+, PHP 8.4+
+ * xtradbrowusers plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
  * Filename: xtradbrowusers.setup.php
  *
- * Date: Jul 16, 2026
+ * Custom Extrafields Users i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+ *
+ * Date: Jul 18, 2026
  * @package xtradbrowusers
- * @version 1.0.0
+ * @version 1.1.1
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowusers-cotonti
  * @license BSD
  */
 
-Cot::$db->registerTable('xtradbrowusers');
+
