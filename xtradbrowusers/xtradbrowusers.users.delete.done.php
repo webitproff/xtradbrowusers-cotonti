1d33566396665d0156e886730be59e34c43bbd33
webitproff <webitproff@gmail.com>
2026-07-18 10:00:45 +0300

    Update xtradbrowusers.users.delete.done.php

--- xtradbrowusers/xtradbrowusers.users.delete.done.php
+++ xtradbrowusers/xtradbrowusers.users.delete.done.php
@@ -11,9 +11,12 @@
  * Удаляет соответствующую запись в таблице cot_xtradbrowusers, а также файлы, загруженные
  * через экстраполя.
  *
- * Date: Jul 17, 2026
+ *
+ * Custom Extrafields Users i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+ *
+ * Date: Jul 18, 2026
  * @package xtradbrowusers
- * @version 1.0.1
+ * @version 1.1.1
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowusers-cotonti
  * @license BSD
