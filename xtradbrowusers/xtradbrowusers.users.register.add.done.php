fe60d202df8e5a0586f8fcbcdb8241d387f649e2
webitproff <webitproff@gmail.com>
2026-08-14 14:05:12 +0300

    Update to version 1.2.9
    Updated file header comments to reflect new version and purpose.

--- xtradbrowusers/xtradbrowusers.users.register.add.done.php
+++ xtradbrowusers/xtradbrowusers.users.register.add.done.php
@@ -6,20 +6,38 @@
 ==================== */
 
 /**
- * Сохранение данных после регистрации пользователя
- * Хук users.register.add.done. Сохраняет значения extrafields плагина xtradbrowusers
- * в таблицу cot_xtradbrowusers после успешной регистрации.
+ * Filename: xtradbrowusers.users.register.add.done.php – Сохранение данных после успешной регистрации пользователя
  *
+ * Purpose:   Подключается к хуку `users.register.add.done`, который вызывается в файле
+ *            /modules/users/inc/users.register.php после успешного создания пользователя.
+ *            Получает ID нового пользователя из переменной $userid, затем импортирует
+ *            и сохраняет все значения экстраполей плагина в таблицу `cot_xtradbrowusers`.
+ *            Для полей типа file дополнительно вызывает `cot_extrafield_movefiles()` (перемещение файла).
  *
- * Custom Extrafields Users i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+ * Мультиязычность на этапе регистрации:
+ *   - Сохраняются только основные значения экстраполей.
+ *   - Переводы в таблицу xtradbrowusers_i18n не записываются.
+ *   - i18n-переводы для новых пользователей могут быть добавлены позже через админ-панель.
  *
- * Date: Jul 18, 2026
+ * Path:     plugins/xtradbrowusers/xtradbrowusers.users.register.add.done.php
+ *
+ * Extrafields Users Custom i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+ *
+ * Source and updates   https://github.com/webitproff/xtradbrowusers-cotonti
+ * ReadMeMore:          https://abuyfile.com/ru/market/cotonti/plugs/extrafields-users-custom
+ * Support:             https://abuyfile.com/ru/forums/cotonti/original/extrafields
+ * API Extrafields:     https://github.com/Cotonti/Cotonti/blob/master/system/extrafields.php
+ *
+ * Date: Aug 14, 2026
+ *
  * @package xtradbrowusers
- * @version 1.1.1
+ * @version 1.2.9
  * @author webitproff
- * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowusers-cotonti
+ * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff
  * @license BSD
  */
+ 
+ 
 defined('COT_CODE') or die('Wrong URL.');
 require_once cot_incfile('xtradbrowusers', 'plug');
 
