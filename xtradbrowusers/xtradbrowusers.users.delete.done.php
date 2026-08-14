cf50ebdba4b8b7636a070ebf028d9be3f52adc0d
webitproff <webitproff@gmail.com>
2026-08-14 13:57:52 +0300

    Update to version 1.2.9
    Updated file metadata and comments for clarity and accuracy.

--- xtradbrowusers/xtradbrowusers.users.delete.done.php
+++ xtradbrowusers/xtradbrowusers.users.delete.done.php
@@ -6,21 +6,42 @@
 ==================== */
 
 /**
- * Файл xtradbrowusers.users.delete.done.php. Удаление связанных данных при удалении пользователя
+ * Filename: xtradbrowusers.users.delete.done.php – Удаление связанных данных при удалении пользователя
  * Хук users.delete.done. Вызывается после удаления пользователя из таблицы cot_users.
- * Удаляет соответствующую запись в таблице cot_xtradbrowusers, а также файлы, загруженные
- * через экстраполя.
+ * Файл с расположением хука: modules/users/inc/UsersControlService.php
+ *             foreach (cot_getextplugins('users.delete.done') as $pl) {
+ *                 include $pl;
+ *             }
+ * Через наш объявленный хук в заголовке xtradbrowusers.users.delete.done.php 
+ * подключаем наш код к файлу UsersControlService.php
+ * Он удаляет соответствующую запись в таблице cot_xtradbrowusers, 
+ * а также файлы, загруженные через экстраполя.
  *
+ * Purpose:   Подключается к хуку `users.delete.done` и выполняет код очистки после удаления пользователя.
+ *            Загружает запись экстраполей пользователя, удаляет связанные файлы (тип `file`)
+ *            через API extrafields, затем удаляет строку из `cot_xtradbrowusers`.
+ *            Благодаря внешнему ключу `ON DELETE CASCADE` переводы из `cot_xtradbrowusers_i18n`
+ *            удаляются автоматически.
  *
- * Custom Extrafields Users i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+ * Path:     plugins/xtradbrowusers/xtradbrowusers.users.delete.done.php
  *
- * Date: Jul 18, 2026
+ * Extrafields Users Custom i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+ *
+ * Source and updates   https://github.com/webitproff/xtradbrowusers-cotonti
+ * ReadMeMore:          https://abuyfile.com/ru/market/cotonti/plugs/extrafields-users-custom
+ * Support:             https://abuyfile.com/ru/forums/cotonti/original/extrafields
+ * API Extrafields:     https://github.com/Cotonti/Cotonti/blob/master/system/extrafields.php
+ *
+ * Date: Aug 14, 2026
+ *
  * @package xtradbrowusers
- * @version 1.1.1
+ * @version 1.2.9
  * @author webitproff
- * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowusers-cotonti
+ * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff
  * @license BSD
  */
+ 
+
 
 defined('COT_CODE') or die('Wrong URL.');
 require_once cot_incfile('xtradbrowusers', 'plug');
