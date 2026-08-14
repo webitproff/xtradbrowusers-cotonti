c676b555ea73e9f7f6f209bbd800f17ef9065f00
webitproff <webitproff@gmail.com>
2026-08-14 13:16:17 +0300

    Update to version 1.2.9

--- xtradbrowusers/xtradbrowusers.extrafields.php
+++ xtradbrowusers/xtradbrowusers.extrafields.php
@@ -6,21 +6,31 @@
 ==================== */
 
 /**
- * xtradbrowusers Plugin. Hooks - admin.extrafields.first, - Register custom table in Extrafields administration
- * Filename: plugins/xtradbrowusers/xtradbrowusers.extrafields.php
+ * Filename: xtradbrowusers.extrafields.php – Register custom table in Extrafields administration
  *
+ * Purpose:   регистрирует таблицу `cot_xtradbrowusers` в списке доступных таблиц
+ *            для управления экстраполями через админ-панель Cotonti.
+ *            Файл выполняется на хуке `admin.extrafields.first` и добавляет
+ *            запись в `$extra_whitelist`, после чего таблица появляется
+ *            в интерфейсе «Extrafields» как объект для добавления/редактирования полей.
  *
- * Custom Extrafields Users i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+ * Path:     plugins/xtradbrowusers/xtradbrowusers.extrafields.php
  *
- * Date: Jul 18, 2026
+ * Extrafields Users Custom i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+ *
+ * Source and updates   https://github.com/webitproff/xtradbrowusers-cotonti
+ * ReadMeMore:          https://abuyfile.com/ru/market/cotonti/plugs/extrafields-users-custom
+ * Support:             https://abuyfile.com/ru/forums/cotonti/original/extrafields
+ * API Extrafields:     https://github.com/Cotonti/Cotonti/blob/master/system/extrafields.php
+ *
+ * Date: Aug 14, 2026
+ *
  * @package xtradbrowusers
- * @version 1.1.1
+ * @version 1.2.9
  * @author webitproff
- * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowusers-cotonti
+ * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff
  * @license BSD
  */
- 
- 
  
 defined('COT_CODE') or die('Wrong URL.');
 require_once cot_incfile('xtradbrowusers', 'plug');
