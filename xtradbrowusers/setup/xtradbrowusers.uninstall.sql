cf322fa32a742a3a099dd2a178e88307f72a5ba3
webitproff <webitproff@gmail.com>
2026-08-14 13:15:37 +0300

    Update to version 1.2.9

--- xtradbrowusers/setup/xtradbrowusers.uninstall.sql
+++ xtradbrowusers/setup/xtradbrowusers.uninstall.sql
@@ -3,11 +3,11 @@
 -- Удаляем таблицу переводов первой, затем основную (из-за внешнего ключа)
 
 -- 
--- Custom Extrafields Users i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+-- Extrafields Users Custom i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
 -- 
--- Date: Jul 18, 2026
+-- Date: Aug 14, 2026
 -- package xtradbrowusers
--- version 1.1.1
+-- version 1.2.9
 -- author webitproff
 -- copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowusers-cotonti
 -- license BSD
